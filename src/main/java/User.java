db59c9683dc1de365569697cc3fe5af5f5ed3c56
Bogdan Yakovlev <bogdanyakovlev539@gmail.com>
2024-12-06 11:43:51 +0300

    User Yakovlev Bogdan

---
+++ src/main/java/User.java
@@ -0,0 +1,40 @@
+public class User {
+    public static void main(String[] args) {
+
+    }
+    public class Userinformation{
+        private String nickname;
+        private String name;
+        private int phonenumber;
+        private int password;
+
+        public Userinformation(){}
+
+        public Userinformation(String nickname, String name,int phonenumber, int password){
+            this.nickname = nickname;
+            this.name = name;
+            this.password = password;
+            this.phonenumber = phonenumber;
+
+
+        }
+
+        public String getNickname() {
+            return nickname;
+
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getPassword() {
+            return password;
+        }
+
+        public void setPhonenumber(int phonenumber) {
+            this.phonenumber = phonenumber;
+        }
+    }
+}
+ 
