5964870898761c6f4a24adb496587b9e45d64663
Bogdan Yakovlev <bogdanyakovlev539@gmail.com>
2024-12-06 23:45:17 +0300

    class User Yakovlev

--- src/main/java/ru/messenger/User.java
+++ src/main/java/ru/messenger/User.java
@@ -1,42 +1,51 @@
 package ru.messenger;
 
 public class User {
-    public static void main(String[] args) {
+    private String nickname;
+    private String name;
+    private int password;
+    private int phonenumber;
+
+    public User(String nickname, String name, int phonenumber, int password) {
+        this.nickname = nickname;
+        this.name = name;
+        this.password = password;
+        this.phonenumber = phonenumber;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+    public void setPassword(int password){
+        this.password = password;
 
     }
-    public class Userinformation{
-        private String nickname;
-        private String name;
-        private int phonenumber;
-        private int password;
 
-        public Userinformation(){}
-
-        public Userinformation(String nickname, String name,int phonenumber, int password){
-            this.nickname = nickname;
-            this.name = name;
-            this.password = password;
-            this.phonenumber = phonenumber;
-
-
-        }
-
-        public String getNickname() {
-            return nickname;
-
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getPassword() {
-            return password;
-        }
-
-        public void setPhonenumber(int phonenumber) {
-            this.phonenumber = phonenumber;
-        }
+    public int getPhonenumber() {
+        return phonenumber;
     }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
 }
+
+
  
