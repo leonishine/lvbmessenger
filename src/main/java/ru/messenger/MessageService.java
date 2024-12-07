ad465d63458ed59cd53df71b29d92829f4375a5d
днс <ivanmaxi52@gmail.com>
2024-12-07 14:32:53 +0900

    work

--- src/main/java/ru/messenger/MessageService.java
+++ src/main/java/ru/messenger/MessageService.java
@@ -1,5 +1,7 @@
 package ru.messenger;
 
+import java.util.Arrays;
+
 public class MessageService {
 
     public MessageService() {}
@@ -7,19 +9,14 @@ public class MessageService {
     public MessageService(User user1, User user2) {
         create4Message(user1, user2);
     }
-    public Message[] arr = new Message[4];
-    public void create4Message(User user1, User user2) {
-        Message m1 = new Message(user1, user2, "Привет, ты как?", "06.12.2024 19:30");
-        arr[0] = m1;
-        Message m2 = new Message(user2, user1, "Привет, отлично. Хочешь отсылку?", "06.12.2024 19:48");
-        arr[1] = m2;
-        Message m3 = new Message(user1, user2, "Давай", "06.12.2024 19:50");
-        arr[2] = m3;
-        Message m4 = new Message(user2, user1, "Прибыл Годжо Сатору", "06.12.2024 20:31");
-        arr[3] = m4;
-    }
 
-    public Message[] getArr() {
+    public static Message[] create4Message(User user1, User user2) {
+        Message[] arr = new Message[4];
+        arr[0] = new Message(user1, user2, "Привет, ты как?", "06.12.2024 19:30");
+        arr[1] = new Message(user2, user1, "Привет, отлично. Хочешь отсылку?", "06.12.2024 19:48");
+        arr[2] = new Message(user1, user2, "Давай", "06.12.2024 19:50");
+        arr[3] = new Message(user2, user1, "Прибыл Годжо Сатору", "06.12.2024 20:31");
+
         return arr;
     }
 }
